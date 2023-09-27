f000bcd9e3d6d939900bdc2224fb4bbf12f6db15
psantoso13 <prayogo.santoso@gmail.com>
2023-09-27 20:43:00 +0700

    initial commit

---
+++ data.js
@@ -0,0 +1,58 @@
+export const tweetsData = [   
+    {
+        handle: `@abstraxdigital`,
+        profilePic: `images/youthone.jpg`,
+        likes: 13,
+        retweets: 111,
+        tweetText: `Web site designer needed urgently!!!`,
+        replies: [
+            {
+                handle: `@Miles`,
+                profilePic: `images/youthtwo.png`,
+                tweetText: `Put me in! I would ❤️ to be part of the team.`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '4b161eee-c0f5-4545-9c4b-8562944223ee',
+    },    
+    {
+        handle: `@Sultan-A ✅`,
+        profilePic: `images/sultanA.png`,
+        likes: 555,
+        retweets: 123,
+        tweetText: `I have one 🚀 for sale, if any of you interested.`,
+        replies: [
+                  {
+                handle: `@Sultan-B ✅`,
+                profilePic: `images/sultanB.png`,
+                tweetText: `Been travel with 🚀 for a while.`,
+            },
+                  {
+                handle: `@Sultan-C ✅`,
+                profilePic: `images/sultanC.jpeg`,
+                tweetText: `Come, ride with me!`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '3c23454ee-c0f5-9g9g-9c4b-77835tgs2',
+    },
+        {
+        handle: `@MySmile`,
+        profilePic: `images/smile.png`,
+        likes: 1,
+        retweets: 0,
+        tweetText: `Who do you think tou are?`,
+        replies: [
+            {
+                handle: `@SkyMan`,
+                profilePic: `images/skyback.png`,
+                tweetText: `Not you!`,
+            },
+        ],
+        isLiked: false,
+        isRetweeted: false,
+        uuid: '8hy671sff-c0f5-4545-9c4b-1237gyys45',
+    },     
+]
